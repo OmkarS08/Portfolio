f563d8b2de3e884e3546b561b06b1d29945560b3
omkar shelke <omkar1144@gmail.com>
2022-12-02 00:55:19 +0400

    Resume Button

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,27 @@
 import './About.css'
-
+import Button from 'react-bootstrap/Button';
 export const About =() =>{
+
+    const onButtonClick = () => {
+        // using Java Script method to get PDF file
+        fetch('Omkar_shelke_Resume.pdf').then(response => {
+            response.blob().then(blob => {
+                // Creating new object of PDF file
+                const fileURL = window.URL.createObjectURL(blob);
+                // Setting various property values
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Omkar_shelke_Resume';
+                alink.click();
+            })
+        })
+    }
+
     return(<div className="About">
     <h1>About me</h1>
-    <img src='/images/cover.png' className='About-cover'></img>
+    <img src='/images/cover.png' className='About-cover' alt='cover'></img>
     <p>Hello, my name is Omkar Shelke. I am currently pursuing a Masters degree at the BITS Pilani (Birla Institute of Technology) Dubai campus.
     <br/>I am a full-stack MERN Developer.</p>
-    <button>Resume/CV</button>
+    <Button onClick={onButtonClick} variant="outline-dark" size="lg">Resume/CV</Button>
     </div>)
 }
