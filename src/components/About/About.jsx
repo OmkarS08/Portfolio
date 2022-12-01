1f8ef9fe488ee22c28c654af4eb66cdbfcc5234c
omkar shelke <omkar1144@gmail.com>
2022-12-02 00:40:09 +0400

    About page start-up

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,11 @@
+import './About.css'
 
 export const About =() =>{
-    return(<div><h1>This is About me</h1></div>)
+    return(<div className="About">
+    <h1>About me</h1>
+    <img src='/images/cover.png' className='About-cover'></img>
+    <p>Hello, my name is Omkar Shelke. I am currently pursuing a Masters degree at the BITS Pilani (Birla Institute of Technology) Dubai campus.
+    <br/>I am a full-stack MERN Developer.</p>
+    <button>Resume/CV</button>
+    </div>)
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,8 @@
 import './Home.css'
 export const Home =() =>{
     return(<div className='Home'> 
-    <h1 className='typewriter'>{`<<Hello World!/>>`}</h1>
+    <h1 className='typewriter'>{`<Hello World!/>`}</h1>
+    <h1 className='typewriter'>{`<I AM Omkar Shelke/>`}</h1>
     <img src="/images/omkar.png" alt='profile' className="profile-photo"/>
     </div>)
 
